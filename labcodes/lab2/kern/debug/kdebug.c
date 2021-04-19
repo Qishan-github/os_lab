db313fcf323c1bcb595c5699157a980af662057d
yyc <1401512756@qq.com>
2021-04-19 20:53:44 +0800

    first commit

--- labcodes/lab2/kern/debug/kdebug.c
+++ labcodes/lab2/kern/debug/kdebug.c
@@ -305,5 +305,21 @@ print_stackframe(void) {
       *           NOTICE: the calling funciton's return addr eip  = ss:[ebp+4]
       *                   the calling funciton's ebp = ss:[ebp]
       */
+
+    uint32_t ebp = read_ebp();
+    uint32_t eip = read_eip();
+    int i;
+    for(i = 0;ebp != 0&&i < STACKFRAME_DEPTH;i++) {
+        // 参数位置定位
+        uint32_t *arguments = (uint32_t*)ebp + 2;
+        // 显示传入的参数
+        cprintf("ebp:0x%08x eip:0x%08x args:0x%08x 0x%08x 0x%08x 0x%08x",ebp,eip,arguments[0],arguments[1],arguments[2],arguments[3]);
+        cprintf("\n");
+        print_debuginfo(eip - 1);
+        // 寻找上一个调用者栈低位置，即模拟函数返回的过程
+        eip = ((uint32_t*)ebp)[1];
+        ebp = ((uint32_t*)ebp)[0];
+    }
+
 }
 
